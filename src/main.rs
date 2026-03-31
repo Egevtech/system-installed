c0be0925a22269d6eb51886a7bb0ab9a01f9fc75
egevtech <egevtech@gmail.com>
2026-04-01 02:34:07 +0300

    Add message when system was born

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{
+    fs::{self, Metadata},
+    path::Path,
+};
+
+use chrono::{DateTime, Utc};
+
 fn main() {
-    println!("Hello, world!");
+    let metadata: Metadata = fs::metadata(Path::new("/")).expect("Failed to get metadata of '/'");
+    let created: DateTime<Utc> = metadata
+        .created()
+        .expect("Failed to get date of birth")
+        .into();
+
+    println!(
+        "System was installed on {}",
+        created.format("%Y-%m-%d, %H:%M:%S")
+    );
 }
